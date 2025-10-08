252b4a075ac220a4caa024b05c7e42ccc7b51bf4
crotobark <gildardo.valencia.h@gmail.com>
2025-10-08 11:44:19 -0500

    creacion de ruats

---
+++ prisma/migrations/20251008155334_prueba_de_migracion/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - Added the required column `hola` to the `products` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_products" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "referencia" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "unidad_medida" INTEGER NOT NULL,
+    "categoria" TEXT NOT NULL,
+    "almacen" TEXT NOT NULL,
+    "stock_minimo" INTEGER NOT NULL,
+    "stock_actual" INTEGER NOT NULL,
+    "hola" TEXT NOT NULL
+);
+INSERT INTO "new_products" ("almacen", "categoria", "description", "id", "referencia", "stock_actual", "stock_minimo", "unidad_medida") SELECT "almacen", "categoria", "description", "id", "referencia", "stock_actual", "stock_minimo", "unidad_medida" FROM "products";
+DROP TABLE "products";
+ALTER TABLE "new_products" RENAME TO "products";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
