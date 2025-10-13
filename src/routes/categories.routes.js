8faaa589aabb8a0e7c9bff63ad739ccfe61119ce
Cookie <camilohurtado256@gmail.com>
2025-10-13 15:24:26 -0500

    migracion a mysql

--- src/routes/categories.routes.js
+++ src/routes/categories.routes.js
@@ -1,9 +1,13 @@
 import {Router} from 'express';
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const router = Router();
 
-router.get('/categories', (req, res) => {
-    res.send('categories');
+router.get('/categories', async (req, res) => {
+    const categories = await prisma.categoria.findMany()
+    res.json(categories)
 });
 
 export default router;
